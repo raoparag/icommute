de6c3baf1fd31b9bd9291a52821b14a606761e4d
raoparag <raoparag@gmail.com>
2015-03-21 13:27:14 -0700

    first commit

---
+++ src/main/java/com/iCommute/Authenticate.java
@@ -0,0 +1,13 @@
+package com.iCommute;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Authenticate {
+	@RequestMapping("/authenticate")
+	public User authenticate(){
+		User user = new User();
+		return user;
+	}
+}
---
+++ src/main/java/com/iCommute/User.java
@@ -0,0 +1,27 @@
+package com.iCommute;
+
+public class User {
+	private String email;
+	private String mobNo;
+	private String defaultLoc;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobNo() {
+		return mobNo;
+	}
+	public void setMobNo(String mobNo) {
+		this.mobNo = mobNo;
+	}
+	public String getDefaultLoc() {
+		return defaultLoc;
+	}
+	public void setDefaultLoc(String defaultLoc) {
+		this.defaultLoc = defaultLoc;
+	}
+
+}
